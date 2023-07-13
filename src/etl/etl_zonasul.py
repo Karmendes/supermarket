31bdb9dd7bef3a65ab4e0b4bb2e8a59146f02bdd
karmendes <lucaseqt@hotmail.com>
2023-07-13 18:31:38 -0300

    feat: Add logic for scrapper

--- src/etl/etl_zonasul.py
+++ src/etl/etl_zonasul.py
@@ -1,18 +1,24 @@
+from os import getenv
+from dotenv import load_dotenv
 from src.etl.etl import ETL
 from src.scrapper.scrap_zona_sul import ScrapperZonaSul
 from src.bigquery.bq_connector import BigQueryConnector
 from src.data_manipulator.main import DataManipulator
 
+load_dotenv()
+URL = getenv("URL_ZONA_SUL")
+
+
 
 
 class ETLZonaSul(ETL):
     def __init__(self):
-        self.extracter = ScrapperZonaSul()
+        self.extracter = ScrapperZonaSul(URL)
         self.manipulator = DataManipulator()
         self.loader = BigQueryConnector()
     def extract(self):
         print('Extraindo dados do zona sul')
-        self.extracter.scrap_date()
+        self.extracter.scrap_data()
     def transform(self):
         print('Transformando dados do zona sul')
         self.manipulator.manipulate_data()
--- src/scrapper/scrap_zona_sul.py
+++ src/scrapper/scrap_zona_sul.py
@@ -1,5 +1,23 @@
-class ScrapperZonaSul:
-    def __init__(self):
-        pass
-    def scrap_date(self):
-        print("Fazendo scrapping dos dados do zona sul")
+from src.scrapper.main import Scrapper
+
+
+class ScrapperZonaSul():
+    def __init__(self,url):
+        self.url = url
+        self.scrapper = Scrapper(self.url)
+        self.data = {}
+    def get_names(self):
+        tag_names = self.scrapper.get_all_tags("span",{"class":"vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body"})
+        self.data['names'] = [x.text for x in tag_names]
+    def get_full_prices(self):
+        tag_prices = self.scrapper.get_all_tags("span",
+        {"class":"zonasul-zonasul-store-0-x-customPricePor vtex-difference__por fw7 f5 text-center flex justify-center"})
+        self.data['integer'] = [self.scrapper.get_single_tag("span",
+        {"class":"zonasul-zonasul-store-0-x-currencyInteger"},x).text for x in tag_prices]
+        self.data['decimal'] = [self.scrapper.get_single_tag("span",
+        {"class":"zonasul-zonasul-store-0-x-currencyFraction"},x).text for x in tag_prices]
+    def scrap_data(self):
+        print("Scraping dos dados do zona sul")
+        self.get_names()
+        self.get_full_prices()
+        return self.data
