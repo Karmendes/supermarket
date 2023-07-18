0bd4560bbe20d6789452a1f6f655e66a68ba1e47
karmendes <lucaseqt@hotmail.com>
2023-07-18 19:06:12 -0300

    feat: Add source code for postgresdb

--- src/db_connector/postgres_connector.py
+++ src/db_connector/postgres_connector.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from src.db_connector.main import DataBaseConnector
 
 class PostgreSQLConnector(DataBaseConnector):
-    def __init__(self):
-        super().__init__(self.user,self.pwd,self.host,self.port,self.database)
+    def __init__(self,user,pwd,host,port,database):
+        super().__init__(user,pwd,host,port,database)
         self.engine = None
         self.session = None
         self.session_maker = None
+        self.connect()
     def connect(self):
         # Lógica específica para a conexão com o PostgreSQL
         self.engine = create_engine(
@@ -17,6 +18,6 @@ class PostgreSQLConnector(DataBaseConnector):
         self.session_maker = sessionmaker(bind=self.engine)
         self.session = self.session_maker()
     def send_data(self,dataframe,table_name,**kwargs):
-        dataframe.to_sql(table_name,self.session,**kwargs)
+        dataframe.to_sql(table_name,self.engine,**kwargs)
     def retrieve_data(self,query):
         raise NotImplementedError()
--- src/loaders/loader_zona_sul.py
+++ src/loaders/loader_zona_sul.py
@@ -1,15 +1,15 @@
 from src.loaders.main import Loaders
-from src.db_connector.sqlite_connector import SQLiteConnector
+from src.db_connector.postgres_connector import PostgreSQLConnector
 
-USER = 'lucas'
-PWD = 'karmendes'
+USER = 'datalake'
+PWD = 'datalake'
 DATABASE = 'datalake'
-PORT = 5555
+PORT = 5432
 HOST = 'localhost'
 
 
 class LoaderZonaSul(Loaders):
     def __init__(self):
-        self.db_conn = SQLiteConnector(USER,PWD,HOST,PORT,DATABASE)
+        self.db_conn = PostgreSQLConnector(USER,PWD,HOST,PORT,DATABASE)
     def load(self,data):
-        self.db_conn.send_data(data,'tb_stg_zona_sul',if_exists='append',index = False)
+        self.db_conn.send_data(data,'tb_zona_sul',if_exists='append',index = False,schema = 'staging')
