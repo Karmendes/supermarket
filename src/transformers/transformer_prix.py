8a800b0cb93be23f9faf175369bbb423972542e9
karmendes <lucaseqt@hotmail.com>
2023-07-19 18:47:26 -0300

    feat: Add tranformer for prix

---
+++ src/transformers/transformer_prix.py
@@ -0,0 +1,12 @@
+from src.transformers.main import Transformer
+from src.data_manipulator.main import DataManipulator,ReadMapReduce
+
+class TransformerPrix(Transformer):
+    def __init__(self):
+        self.manipulator = DataManipulator(ReadMapReduce)
+    # Read data
+    def transform(self,data):
+        self.manipulator.read_data(data)
+        self.manipulator.mark_timestamp()
+        self.manipulator.mark_source('Prix')
+        return self.manipulator.data
