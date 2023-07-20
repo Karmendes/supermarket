32175951977cb84020e9c8e9463fe4fe0a929320
karmendes <lucaseqt@hotmail.com>
2023-07-20 14:27:28 -0300

    feat: Add Airflow

---
+++ infra/airflow/dags/hook/hook_requests.py
@@ -0,0 +1,8 @@
+
+import requests
+
+
+def call_api(supermarket):
+    endpoint = f'http://localhost:5000/{supermarket}'
+    response = requests.get(endpoint,timeout= 60)
+    return response.status_code
