1fbf9e8cfc86fbd5c007292f746e0236a581af62
karmendes <lucaseqt@hotmail.com>
2023-07-25 11:16:14 -0300

    config: change path for api

--- infra/airflow/dags/hook/hook_requests.py
+++ infra/airflow/dags/hook/hook_requests.py
@@ -3,6 +3,6 @@ import requests
 
 
 def call_api(supermarket):
-    endpoint = f'http://api:5000/{supermarket}'
+    endpoint = f'http://api-flask-service:5000/{supermarket}'
     response = requests.get(endpoint,timeout= 60)
     return response.status_code
